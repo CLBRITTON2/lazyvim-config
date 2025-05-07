7d73330af4c04c6e51cb4c07d66f844bfaf605e6
CLBRITTON2 <christopher.l.britton@outlook.com>
2025-05-07 16:52:38 -0400

    disable bufferline, add harpoon and harpoon files, add harpoon files to lualine
    add work around for breaking mason update... nice

---
+++ lua/plugins/harpoon-files.lua
@@ -0,0 +1,15 @@
+return {
+  "kiennt63/harpoon-files.nvim",
+  dependencies = {
+    { "ThePrimeagen/harpoon" },
+  },
+  opts = {
+    max_length = 10,
+    icon = "",
+    show_icon = true,
+    show_index = true,
+    show_filename = true,
+    separator_left = " ",
+    separator_right = " ",
+  },
+}
---
+++ lua/plugins/mason-workaround.lua
@@ -0,0 +1,5 @@
+return {
+  -- Mason released breaking changes - prob won't be fixed any time soon https://github.com/LazyVim/LazyVim/issues/6039#issuecomment-2856227817
+  { "mason-org/mason.nvim", version = "1.11.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
+}
